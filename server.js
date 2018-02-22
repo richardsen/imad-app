46f8bf2eae142e0dcd011b603b3c72aa90d48c21
richardsen <richardsen2493@gmail.com>
2018-02-22 22:50:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content={
+var articleOne={
     
     title: 'Article-one',
     heading:'Article-one',
@@ -68,7 +68,7 @@ var htmlTemplate={`
 </div>
 </body>
 </html>
-`:
+`;
     return htmlTemplate;
 }
 
