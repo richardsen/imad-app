bb326f15e65db03b025df2e7ca4e0938e3d24f05
richardsen <richardsen2493@gmail.com>
2018-02-22 16:28:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-one', function(req,res){
-res.sendFile('Article-one');
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-one', function(req,res){
-res.sendFile('Article-one');
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-app.get('/article-one', function(req,res){
-res.sendFile('Article-one');
+
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
