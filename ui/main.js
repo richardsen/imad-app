39f588a8d37122538b8054b82c2647468cb25d8f
richardsen <richardsen2493@gmail.com>
2018-02-23 00:29:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,12 @@ var element= document.getElementById("main-text");
 element.innerHTML = 'What the cluck bro?????';
 
 var img= document.getElementById('madi');
-
+var marginLeft=0;
+function marginRight () {
+    marginLeft= marginLeft + 10;
+    img.style.marginLeft=marginLeft + 'px';
+}
 img.onclick= function () {
-    var inte=setInterval(moveLeft,100);
+    var inte=setInterval(moveRight,100);
     img.style.marginLeft='1000px';
 };
