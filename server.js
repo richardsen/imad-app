42aba090ac015d1f485fb5b4ea2d8bdead3f4f51
richardsen <richardsen2493@gmail.com>
2018-02-22 17:26:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplae(articleOne));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
