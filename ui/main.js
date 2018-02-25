9b49bd5dda8062a54b9e404c2c7e7e0038f476a3
richardsen <richardsen2493@gmail.com>
2018-02-25 14:26:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ var name= nameInput.value();
 var submit=document.getElementById('submit_button');
 submit.onclick= function(){
     //make request to server and send name, capture the names and render as a list
-    var names= [name1,name2,name3];
+    var names= ['name1','name2','name3'];
     var list='';
     for(var i=0; i<=names.length;i++){
         list +='<li>'+ names(i) '</li>';
