99dd9687212a6615e188ed052d7514e0b17cbd15
richardsen <richardsen2493@gmail.com>
2018-02-25 13:50:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,17 @@ var button=document.getElementById("counter");
 var counter=0;
 button.onclick= function () {
   //Make a request 
-  
-  // Capture response and store it
-  
-  //Render the variable in the correct span
-  counter=counter +1; 
-  var span= document.getElementById('count'); 
-  span.innerHTML= counter.toString();
-  
-    
+  var request= new XMLhttpRequest();
+  request.onreadystatechange= function (){
+      if(request.readyState==XMLhttpRequest.DONE){
+          if(request.status==200){
+              var counter=respond.responseText;
+              var span=document.getElementById();
+              span.innerHTML=counter.toString();
+          }
+      }
+      
+};
+    request.open('GET', 'https://http://richardsen2493.imad.hasura-app.io/', true);
+    request.send(null);
 };
