961358482379f5e06a4a3abb4bbe1ad28f4a6704
richardsen <richardsen2493@gmail.com>
2018-02-25 12:59:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var counter=0;
-app.get(':/counter', function (req,res){
+app.get('/counter', function (req,res){
     x=x+1;
     res.send(counter.toString());
 });
